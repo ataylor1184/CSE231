ddbb01956ea1a319d45b276df06506d44349cf3c
ataylor1184 <69189089+ataylor1184@users.noreply.github.com>
2020-08-04 03:48:39 -0400

    Add files via upload

---
+++ Proj01/proj01.py
@@ -0,0 +1,17 @@
+m = input("Input the first integer: ")
+for count in range(1,int(m)+1): 
+    if (int(m) % count == 0):
+     gcd1 = count
+     print(gcd1)
+        
+        
+        
+        
+n = input("Input the second integer: ")
+for count2 in range(1,int(n)+1): 
+    if (int(n) % count2 == 0):
+     gcd2 = count2
+     print(gcd2)
+        
+        
+        
